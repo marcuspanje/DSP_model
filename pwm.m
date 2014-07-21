68f874488a02c67cb64c364671f676866ff56156
Marcus Pan <marcuspanje@gmail.com>
2014-07-21 18:20:59 -0400

    pwm takes in 2 freqs now - but prob will revert back

--- pwm.m
+++ pwm.m
@@ -3,12 +3,17 @@
 %swth = sawtooth wave to be used as comparator
 %Vdc = offset V added on to input sig
 
-function [Y] = pwm(X, swth, high, Vdc)
-n = length(swth);
+function [Y] = pwm(X, swth, high, Vdc, fsig, fswth)
+n = length(X);
 Y = high*ones(n, 1);
 
 for i=1:n;
-    if swth(i) < abs(X(i)+Vdc)
+    t = (i-1)/fsig;
+    i_swth = 1 + round(t*fswth);
+        
+    if swth(i_swth) < abs(X(i)+Vdc)
         Y(i) = 0;
     end
+        
+end
 end
